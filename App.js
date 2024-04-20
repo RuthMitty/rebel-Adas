97146906cec81b73f41c8408cbf1317b768e1b68
NELLY CASTILLO CADENAS <99703789+Nelly-Castillo@users.noreply.github.com>
2024-04-20 08:30:00 -0600

    Add Reportes

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { createNativeStackNavigator} from '@react-navigation/native-stack';
 import SolicitudAbas, { SOLICITUD_ABAS } from "./src/components/pages/abastecimiento/solicitudAbas";
 import ReaAbas, { REA_ABAS } from "./src/components/pages/abastecimiento/ReaAbas";
 import Manuals, { MANUALS } from "./src/components/pages/Manuales";
+import Reports, { REPORTS} from "./src/components/pages/Reportes";
 import HomeAbas from "./src/components/pages/abastecimiento/HomeAbas";
 // import Menu from "./src/components/pages/abastecimiento/MenuAbas";
 // import TopBar from './src/components/TopBar';
@@ -24,6 +25,7 @@ export default function App() {
       <Stack.Screen name="Home Abastecimiento" component={HomeAbas} />
         <Stack.Screen name={SOLICITUD_ABAS} component={SolicitudAbas} />
         <Stack.Screen name={MANUALS} component={Manuals} />
+        <Stack.Screen name={REPORTS} component={Reports} />
         <Stack.Screen name={REA_ABAS} component={ReaAbas} />
       </Stack.Navigator>
     </NavigationContainer>
